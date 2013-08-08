12b757bb0108291bfc3419c2f09a877c729a4136
Alexander Conroy <alex@esotech.org>
2013-08-08 18:48:23 -0400

    Delete CustomLogin.php

--- CustomLogin.php
+++
@@ -1,8 +0,0 @@
-add_action('login_head', 'wph_custom_login_logo');
-if ( !function_exists('wph_custom_login_logo') )
-{
-  function wph_custom_login_logo() 
-	{
-	    echo '<style type="text/css">h1 a { background-image:url(' . get_stylesheet_directory_uri() .'/images/esotech-wordpress-logo.png) !important; opacity:0.7; height: 47px !important;}</style>';
-	}
-}
